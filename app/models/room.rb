cfa7710c0fe985a2d1d836e8137ecf2545596c35
gnestc <gnest@hotmail.ca>
2021-06-25 16:07:31 -0400

    Add search filters

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,4 +6,11 @@ class Room < ActiveRecord::Base
   has_many :invoice_details, as: :owner
   belongs_to :room_type
   belongs_to :view
+
+  scope :search_date, ->(start_date, end_date) { joins(:reservations_rooms, reservations_rooms: :reservation)\
+    .where("reservations.end_date < ?", start_date)\
+    .where("reservations.start_date > ?", end_date)}
+  scope :never_reserved, -> { where("id NOT IN (SELECT reservations_rooms.room_id FROM reservations_rooms)") }
+  scope :by_view, ->(view_id) { where(view_id: view_id) if view_id.present? }
+  scope :by_type, ->(type_id) { where(room_type_id: type_id) if type_id.present? }
 end
